dc44a6fb7495dc34e81585d02349524579843c8c
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-01-19 00:59:10 +0300

    Add new comet app

---
+++ NewCometApp/NewCometApp/NewCometApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+using Microsoft.Maui.Hosting;
+
+namespace NewCometApp;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)	: base(handle, ownership)
+	{
+	}
+	protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
+}
+
---
+++ NewCometApp/NewCometApp/NewCometApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui.Hosting;
+
+namespace NewCometApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => App.CreateMauiApp();
+}
+
+

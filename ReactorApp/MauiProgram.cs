360fce795e4c27a938cf1d4417f7f59669418b8b
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-07-31 18:44:54 +0300

    Init empty reactor-maui project

---
+++ ReactorApp/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using MauiReactor;
+using ReactorApp.Pages;
+
+
+namespace ReactorApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiReactorApp<MainPage>(app =>
+            {
+                app.AddResource("Resources/Styles/Colors.xaml");
+                app.AddResource("Resources/Styles/Styles.xaml");
+
+                app.SetWindowsSpecificAssectDirectory("Assets");
+            })
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+            });
+
+        return builder.Build();
+    }
+}
